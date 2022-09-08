018f6522b20033ff66e0d14da3162d44c00a3fb5
Naomi Kleinhans <naomikleinhans56@gmail.com>
2022-09-08 05:37:23 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
- *
  * main - Entry point
  *
- *   
- *
- * Return: Always 0 (Success)
+ * description : 'display a string using the puts method'
  *
  */
 
 int main(void)
-
-{puts("\"Programming is like building a multilingual puzzle");
-
-	    return (0);
-
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
