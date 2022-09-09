b52a7220f0852783ca5d21b8f7fcf670c49e74b4
Naomi Kleinhans <naomikleinhans56@gmail.com>
2022-09-09 04:41:55 -0700

    8 Base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,13 @@
-#include <stdio.h>                                                             
-/**                                                                           
+#include <stdio.h>
+/**
  * main - Entry point
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 
-{                                                                            
-char base_16;                                                                
+{
+char base_16;
 
 for (base_16 = 48; base_16 <= 57; base_16++)
 {
