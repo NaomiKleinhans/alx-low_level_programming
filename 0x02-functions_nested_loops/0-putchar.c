2df7753893db8c48f1113dfc1eb5c359f7a7a080
Naomi Kleinhans <naomikleinhans56@gmail.com>
2022-09-13 06:49:52 -0700

    0 Print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * Main - Entry point
+ * Main - Entry point (Print putchar)
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	putchar("_putchar\n");
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
