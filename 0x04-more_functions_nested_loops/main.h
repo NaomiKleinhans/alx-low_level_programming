38bc80e33efc292391dafeec2bd9dd0d246aa6e5
Naomi Kleinhans <naomikleinhans56@gmail.com>
2022-09-15 01:45:01 -0700

    Main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifdef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -7,5 +8,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
 
 #endif
