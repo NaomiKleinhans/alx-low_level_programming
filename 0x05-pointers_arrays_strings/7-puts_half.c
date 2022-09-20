324cee20d5680fc8392114a76b01252f60ba3ab1
Naomi Kleinhans <naomikleinhans56@gmail.com>
2022-09-20 06:40:16 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,26 @@
 
 void puts_half(char *str)
 {
-	int x, y, i;
+	int j = 0;
+	int k;
 
-	x = strlen(str);
-	if (x % 2 == 1)
-		y = x / 2 + 1;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
 	else
-		y = x / 2;
-	for (i = y; i < x; i++)
-	puts("%s", *str / 2);
-	puts("%s\n");
+	{
+		k = j / 2;
+	}
+	for (; k < j; k++)
+	{
+		putchar(str[k]);
+	}
+
+	putchar('\n');
 }
