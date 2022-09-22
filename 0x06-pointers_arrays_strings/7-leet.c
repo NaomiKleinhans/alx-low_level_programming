d434805e566b7c8045625dcd237591d6de7aaf2f
Naomi Kleinhans <naomikleinhans56@gmail.com>
2022-09-22 14:54:30 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - unction that encodes a string into 1337
+ * @src: String to encode
+ *
+ * Return: The encoded string
+ */
+char *leet(char *src)
+{
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	chear n[5] = {'4', '3', '0', '7', '1'};
+
+	while (src[a])
+	{
+		b = 0;
+		
+		while (b < l)
+		{
+			if (src[a] == r[b] || src[a] - 32 == r[b])
+			{
+				src[a] = n[b];
+			}
+			b++;
+		}
+		a++;
+	}
+	return (src)
+}
