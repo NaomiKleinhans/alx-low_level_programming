999ff8ba6476781c3cc4e864ce4f9ac7c47825c2
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-09-24 14:10:43 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,8 +13,8 @@ int main(void)
 
 	for (a = 0; a < 8; a++)
 	{
-		for (b = a +1; b < 9; b++)
-		{ 
+		for (b = a + 1; b < 9; b++)
+		{
 			for (c = b + 1; c < 10; c++)
 			{
 				putchar((a % 10) + '0');
