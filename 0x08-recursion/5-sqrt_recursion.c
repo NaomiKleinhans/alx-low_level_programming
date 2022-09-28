29c83b4cb99ea5655ac612bffc8a6a98ba4419c9
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-09-28 07:58:24 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ *
+ * @n: number integer
+ * Return: To 1 on success and -1 on error
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
