55fef5fc4845458507068126bbf40ddf212256d6
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-09-28 08:06:34 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 
 /**
+ * sqrt2 - Elevate from 1 to n
+ * @i: same as n
+ * @j: interates number from 1 to n
+ * Return: 1 on Success
+ */
+int sqrt2(int i, int j)
+{
+	if (j * j == i)
+		return (j);
+	else if (j * j > i)
+		return (-1);
+	return (sqrt2(i, j +1));
+}
+/**
  * _sqrt_recursion -  returns the natural square root of a number
  *
  * @n: number integer
