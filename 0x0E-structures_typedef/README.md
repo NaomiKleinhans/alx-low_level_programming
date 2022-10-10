c8672895e2fcc739d55d726d43bed08a7cb72451
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-10-10 14:21:55 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+ALX project 0x0E.C - Structures, typedef. 
+What structures are, when, why and how to use them.
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOGH
+#define DOGH
+/**
+ * struct dog - a dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+/* typedef is reserved keyword that gives an existing datatype a "name"*/
+
+
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+typedef dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+int _putchar(char c);
+
+#endif
