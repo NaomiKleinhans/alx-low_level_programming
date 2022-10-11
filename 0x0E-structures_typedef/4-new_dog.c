ce87eab1aea3057e1cba64afa1314371ea0b964d
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-10-11 06:15:16 -0700

    correcting

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,15 +1,18 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 	int strlen(char name, char owner)
-        char *strcpy(char name, char owner);
-	
+	char *_strcpy(char name, char owner);
+
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner
+ * @strlen: array length of string
+ * @strcpy: Copy string of arrays
  * Return: NULL if fail
  */
 
@@ -17,22 +20,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 
 	dog_t *dognew;
+
 	int name_1 = 0, own_1 = 0;
 
 	if (name == NULL || age < 0 || owner == NULL)
+	{
 		return (NULL);
-	dognew = malloc(sizeof(dog_t));
-	if (dognew == NULL)
-		return (NULL);
+	}
 
-	dognew->name = malloc(sizeof(char) * (strlen(name) + 1));
+	dognew = malloc(sizeof(dog_t));
+
+	if (dognew == NULL)
+	{
+		return (NULL);
+	}
+
+	dognew->name = malloc(sizeof(char) * (_strlen(name) + 1));
+
 	if (dognew->name == NULL)
 	{
 		free(dognew);
 		return (NULL);
 	}
 
-	dognew->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	dognew->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
 	if (dognew->owner == NULL)
 	{
 		free(dognew->name);
@@ -40,9 +52,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	}
-	dognew->name = strcopy(dognew->name, name);
+	dognew->name = _strcopy(dognew->name, name);
 	dognew->age = age;
-	dognew->owner = strcopy(dognew->owner, owner);
+	dognew->owner = _strcopy(dognew->owner, owner);
 
 	return (dognew);
 }
