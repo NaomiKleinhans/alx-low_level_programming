aec489bea56e19eeba427c1a846a441f3eef8dde
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-10-24 04:46:04 -0700

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+}
