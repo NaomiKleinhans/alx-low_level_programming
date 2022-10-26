20ab3e3706c87e861184ad988d05727537f70eb8
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-10-26 05:00:56 -0700

    correction 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,13 +13,47 @@
 size_t print_listint(const listint_t *h)
 {
 
-	size_t nodes = 0;
+	int i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
+		i++;
 	}
-	return (nodes);
+	return (i);
+}
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	listint_t *head;
+	listint_t *new;
+	listint_t hello = {8, NULL};
+	size_t n;
+
+	head = &hello;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	new->next = head;
+	head = new;
+	n = print_listint(head);
+	printf("->%lu elements\n", n);
+	free(new);
+	return (0);
 }
