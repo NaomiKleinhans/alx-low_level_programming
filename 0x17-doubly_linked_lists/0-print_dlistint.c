741f404a8ea1df6c0c21f5aa89cb301f3a578e27
NaomiKleinhans <naomikleinhans56@gmail.com>
2022-12-08 22:17:58 +0000

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,13 +7,13 @@
  * Return: number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 
 	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n, h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 
