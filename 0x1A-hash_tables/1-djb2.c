901a8c193d17253ec8dd3e42370c41e7a4e2c7a8
NaomiKleinhans <naomikleinhans56@gmail.com>
2023-01-08 08:19:44 +0000

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,13 +1,11 @@
 #include "hash_tables.h"
 
 /**
-  * hash_djb2 - function generating a random number through hash
-  * number given.
-  * 
-  * @str: string
-  * Return: hash number
-  */ 
-
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
